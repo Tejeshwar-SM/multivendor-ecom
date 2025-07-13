b118af1d8bcaba8e5c4be1668a1fc4145caf3c31
Tejeshwar <work10tejeshwar@gmail.com>
2025-07-13 13:51:48 +0530

    setup3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import {Button} from "@components/ui/button.tsx"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <Button>Click Me</Button>
     </div>
- );
+  );
 }
